54e926dedcec37f9b828b0a5c684a2a1ad3b6b97
Tamer <allwin2002au@gmail.com>
2014-04-04 00:03:33 +1100

    silly simple app

---
+++ js/silly.js
@@ -0,0 +1,14 @@
+/**
+ * @author tamer
+ */
+
+var SILLY = (
+	function(module){
+	module.DoIt = function(resultObject){
+		resultObject.prepend(Date()+'<br />');
+	};
+	return module;	
+	}(SILLY || {})
+	
+	);
+
